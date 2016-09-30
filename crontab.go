b966c8a2e83b0025378b552d905f83358509fa0a
Joshua Steven Theopillus <joshua.steven@tokopedia.com>
2016-09-30 20:54:01 +0700

    modify crontab

---
+++ crontab.go
@@ -0,0 +1,163 @@
+package gover
+
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
+//containers for all gotermins
+//can be used as main controller or to retain informations
+type CrontabMinE struct {
+	//list of registered gotermins in form of a map
+	//the map key is the register name
+	cronjobs map[string]*Gotermin
+	//the set timezone
+	//all gotermins will run in this timezone
+	timeLocation *time.Location
+}
+
+//create new container with a certain time location
+//return error if time location is empty
+func NewCrontab(loc *time.Location) (*CrontabMinE, error) {
+	if loc == nil {
+		return nil, fmt.Errorf("Please input a valid time location")
+	}
+
+	return &CrontabMinE{
+		cronjobs:     map[string]*Gotermin{},
+		timeLocation: loc,
+	}, nil
+}
+
+//register gotermins on the crontab with key
+//the requirement is exactly the same for each category
+//only this time use location from crontab
+//return error if failed to create the gotermin
+func (ct *CrontabMinE) RegisterNewHourly(key string, job func(context.Context), minute string) error {
+	return ct.registerNew("hourly", key, job, minute)
+}
+
+func (ct *CrontabMinE) RegisterNewDaily(key string, job func(context.Context), hour string) error {
+	return ct.registerNew("daily", key, job, hour)
+}
+
+func (ct *CrontabMinE) RegisterNewCustomInterval(key string, job func(context.Context), customInterval time.Duration) error {
+	return ct.registerNew("custom", key, job, customInterval)
+}
+
+func (ct *CrontabMinE) registerNew(cat, key string, job func(context.Context), input interface{}) error {
+	//return error if duplicate key is found
+	if _, ok := ct.cronjobs[key]; ok {
+		return fmt.Errorf("Duplicate key for %s is found", key)
+	}
+
+	//create new gotermin, return error if failed to
+	//switch the category, also return return error if category is invalid
+	var gotermin *Gotermin
+	var err error
+
+	switch cat {
+	case "hourly":
+		if minute, ok := input.(string); !ok {
+			return fmt.Errorf("Invalid input type")
+		} else {
+			gotermin, err = NewHourly(job, minute, ct.timeLocation)
+
+		}
+	case "daily":
+		if hour, ok := input.(string); !ok {
+			return fmt.Errorf("Invalid input type")
+		} else {
+			gotermin, err = NewDaily(job, hour, ct.timeLocation)
+		}
+	case "custom":
+		if customInterval, ok := input.(time.Duration); !ok {
+			return fmt.Errorf("Invalid input type")
+		} else {
+			gotermin, err = NewCustomInterval(job, customInterval, ct.timeLocation)
+		}
+	default:
+		return fmt.Errorf("Invalid category")
+	}
+
+	if err != nil {
+		return err
+	}
+
+	//if there's no error then add the key into crontab
+	ct.cronjobs[key] = gotermin
+	return nil
+}
+
+//start all inactive gotermins
+//return error if any of them is failing
+func (ct *CrontabMinE) StartAll() error {
+	for _, gotermin := range ct.cronjobs {
+		if !gotermin.isActive {
+			if err := gotermin.Start(); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+//start a certain gotermin
+//return error if key is not found or failed to start
+func (ct *CrontabMinE) Start(key string) error {
+	gotermin, found := ct.cronjobs[key]
+	if !found {
+		return fmt.Errorf("Key %s is not found", key)
+	}
+
+	return gotermin.Start()
+}
+
+//stop all active gotermins
+//return error if any of them is failing
+func (ct *CrontabMinE) StopAll() {
+	for _, gotermin := range ct.cronjobs {
+		go gotermin.Stop()
+	}
+}
+
+//stop a certain gotermin
+//return error if key is not found or failed to stop
+func (ct *CrontabMinE) Stop(key string) error {
+	gotermin, found := ct.cronjobs[key]
+	if !found {
+		return fmt.Errorf("Key %s is not found", key)
+	}
+
+	return gotermin.Stop()
+}
+
+//return the summary of current crontab
+func (ct CrontabMinE) Print() string {
+	result := fmt.Sprintf(`
+Summary
+Key-----Interval-----StartingPoint-----Status`)
+
+	for key, cronjob := range ct.cronjobs {
+		interval := cronjob.intervalCategory
+		if interval == "custom" {
+			interval += fmt.Sprintf(" (%+v)", cronjob.customInterval)
+		}
+
+		isActive := "inactive"
+		if cronjob.isActive {
+			isActive = "active"
+		}
+
+		startingPoint := cronjob.startingPoint
+		if startingPoint == "" {
+			startingPoint = "immediately"
+		}
+
+		result += fmt.Sprintf(`
+%s-----%s-----%s-----%s`, key, interval, startingPoint, isActive)
+	}
+
+	return result
+}
---
+++ crontab_test.go
@@ -0,0 +1,151 @@
+package gover
+
+import (
+	"context"
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"sync/atomic"
+	"testing"
+	"time"
+)
+
+func TestCreateNewCrontab(t *testing.T) {
+	_, err := NewCrontab(nil)
+	assert.Error(t, err)
+
+	jkt, _ := time.LoadLocation("Asia/Jakarta")
+	result, err := NewCrontab(jkt)
+	assert.NoError(t, err)
+	assert.Equal(t, jkt, result.timeLocation)
+}
+
+type Cat struct {
+	Name string
+	Race string
+	Age  int64
+}
+
+func (c *Cat) meowing(ctx context.Context) {
+	fmt.Printf("%s a %d old %s cat says 'Meow!'\n'", c.Name, c.Age, c.Race)
+}
+
+func (c *Cat) aging(ctx context.Context) {
+	currentAge := c.Age
+	newAge := atomic.AddInt64(&currentAge, 1)
+	c.Age = newAge
+}
+
+func TestRegisterAndStartNew(t *testing.T) {
+	jkt, _ := time.LoadLocation("Asia/Jakarta")
+
+	crontab, _ := NewCrontab(jkt)
+	defer crontab.StopAll()
+
+	addie := Cat{"Addie", "Siammese", 3}
+
+	err := crontab.RegisterNewHourly("addie", addie.meowing, "123")
+	assert.Error(t, err)
+
+	err = crontab.RegisterNewHourly("addie", addie.meowing, "01")
+	assert.NoError(t, err)
+
+	err = crontab.RegisterNewHourly("addie", addie.meowing, "01")
+	assert.Error(t, err)
+
+	lorrie := Cat{"Lorrie", "Russian Blue", 5}
+	err = crontab.RegisterNewHourly("lorrie", lorrie.meowing, "")
+	assert.NoError(t, err)
+
+	err = crontab.Start("eddie")
+	assert.Error(t, err)
+
+	err = crontab.StartAll()
+	assert.NoError(t, err)
+
+	time.Sleep(time.Millisecond * 500)
+
+	err = crontab.Start("addie")
+	assert.Error(t, err)
+
+	err = crontab.Stop("lorry")
+	assert.Error(t, err)
+
+	err = crontab.Stop("lorrie")
+	assert.NoError(t, err)
+
+	crontab.StopAll()
+
+	lowell := Cat{"Lowell", "Amber", 7}
+	err = crontab.RegisterNewDaily("lowell", lowell.meowing, "01")
+	assert.Error(t, err)
+
+	err = crontab.RegisterNewDaily("lowell", lowell.meowing, "0530")
+	assert.NoError(t, err)
+
+	err = crontab.RegisterNewDaily("lorrie", lowell.meowing, "0530")
+	assert.Error(t, err)
+
+	err = crontab.RegisterNewDaily("lowell2", lowell.meowing, "")
+	assert.NoError(t, err)
+
+	err = crontab.Start("lowell2")
+	assert.NoError(t, err)
+
+	time.Sleep(time.Millisecond * 500)
+
+	err = crontab.Start("lowell2")
+	assert.Error(t, err)
+
+	moritz := Cat{"Moritz", "Bengal", 1}
+	err = crontab.RegisterNewCustomInterval("lowell", moritz.meowing, time.Second*2)
+	assert.Error(t, err)
+
+	err = crontab.RegisterNewCustomInterval("moritz", moritz.meowing, time.Second*2)
+	assert.NoError(t, err)
+
+	err = crontab.RegisterNewCustomInterval("moritz2", moritz.meowing, time.Millisecond*999)
+	assert.Error(t, err)
+
+	err = crontab.Start("moritz")
+	assert.NoError(t, err)
+
+	time.Sleep(time.Millisecond * 500)
+
+	err = crontab.Start("moritz")
+	assert.Error(t, err)
+
+	crontab.StopAll()
+
+	time.Sleep(time.Millisecond * 500)
+
+	err = crontab.RegisterNewCustomInterval("addie_age", addie.aging, time.Second)
+	assert.NoError(t, err)
+
+	err = crontab.RegisterNewCustomInterval("moritz_age", moritz.aging, time.Second*2)
+	assert.NoError(t, err)
+
+	err = crontab.Start("addie_age")
+	assert.NoError(t, err)
+
+	err = crontab.Start("moritz_age")
+	assert.NoError(t, err)
+
+	time.Sleep(time.Second * 3)
+
+	err = crontab.Stop("addie_age")
+	assert.NoError(t, err)
+
+	time.Sleep(time.Second * 2)
+
+	err = crontab.Stop("moritz_age")
+	assert.NoError(t, err)
+
+	assert.Equal(t, int64(6), addie.Age)
+	assert.Equal(t, int64(4), moritz.Age)
+
+	assert.Equal(t, 7, len(crontab.cronjobs))
+
+	summary := crontab.Print()
+	fmt.Println(summary)
+	assert.NotEqual(t, "", summary)
+}
